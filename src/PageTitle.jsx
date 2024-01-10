35a6384e5cc6884ee1caef50c2072df04373fc90
Mallikarjuna Maddi <rammallikarjunarao@gmail.com>
2024-01-10 16:32:27 +0530

    Feat: Created a title component and presented the dynamic data rendering in JSX.

---
+++ src/PageTitle.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function PageTitle() {
+    const title = "React Scratch App";
+    return (
+        <h1>{title}</h1> // {} helps us to mix up js in html and to render dynamic content.
+    );
+}
+
+export default PageTitle;

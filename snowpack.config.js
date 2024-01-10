dcd60a5115b8db8efbf4d800457c93d7baba2d26
Mallikarjuna Maddi <rammallikarjunarao@gmail.com>
2024-01-10 16:36:40 +0530

    Updated the description.

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     mount: {
-        "public": "/", // This tells snowpack to use the public folder path as root of the appilcation
+        "public": "/", // This tells snowpack to use the public folder path as root of the appilcation and looks for the index.htlm file in it
         "src": "/dist" // This tells snowpack to use the src folder path as virtual location for the js, HTML files it will generate
     }
 }

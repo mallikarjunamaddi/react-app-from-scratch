72157d977e74bb3f806021e84d8483258cf8c115
Mallikarjuna Maddi <rammallikarjunarao@gmail.com>
2024-01-10 17:41:57 +0530

    Updated comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import PageTitle from "./PageTitle";
-import "./index.css";
+import "./index.css"; // This is how we import CSS files in React.
 
 function App() {
     return (
         <article>
-            <PageTitle />
+            {/* This indicates using a component inside another component. */}
+            <PageTitle /> 
             <p>Built the app from the scratch.</p>
         </article>
     );
--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
 import React from "react"; //import this to use JSX syntax.
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom"; //import this to render react application inside HTML element.
 import App from "./App";
 
 //render() renders react application inside the HTML element with id 'app'

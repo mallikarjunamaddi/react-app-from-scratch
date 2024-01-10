ef498a013fed64445ed022887519cd1a4d1cc853
Mallikarjuna Maddi <rammallikarjunarao@gmail.com>
2024-01-10 16:33:56 +0530

    Feat: Created an app component and incllude title coponent in it and added stylings as well.

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import PageTitle from "./PageTitle";
+import "./index.css";
+
+function App() {
+    return (
+        <article>
+            <PageTitle />
+            <p>Built the app from the scratch.</p>
+        </article>
+    );
+}
+
+export default App;
